23cbf8d40916035687d3b252d2196bd3e2c83151
Rani Zouaoui <88784948+ranizouaoui@users.noreply.github.com>
2024-04-11 23:08:05 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'/',
+  base:'/Garderie/',
   resolve: {
     alias: [
       { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
